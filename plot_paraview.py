834985a5ae2af7bd1eeef79a9917bb3869935460
Pierre Seize <pierre.seize@onera.fr>
2022-07-07 10:26:26 +0200

    Cleanup

--- plot_paraview.py
+++ plot_paraview.py
@@ -1,7 +1,6 @@
-import numpy as np
+import os
 
 import pvlib
-import os
 
 if __name__ == '__main__':
     def cpath(name, volu=True):
@@ -13,18 +12,9 @@ if __name__ == '__main__':
 
 
     plotter = pvlib.SpherePlotter(view_size=(600, 600), time=False, use_angle=True,
-                                  ray_array="P", surface_array='P')
+                                  ray_array="P", surface_array='Flux~de~chaleur')
 
-    # plotter.register_plot(cpath("JFNK_NS/RUN_KEX", True), 'P', view_size=(800, 800))
-    plotter.register_surface(cpath("BASE_NS/RUN_KEX", False), label='Classical method', ms=1)
-    plotter.register_surface(cpath("JFNK_NS/RUN_KEX", False), label='JFNK method', ms=2)
-    plotter._current_color = 0
-    plotter.register_ray(cpath("BASE_NS/RUN_KEX", True), [0,], ms=1, label='Classical method')
-    plotter.register_ray(cpath("JFNK_NS/RUN_KEX", True), [0,], ms=2, ls=1, label='JFNK method')
-
-    plotter.draw(duration=0, block=True)
-
-    # plotter.compute_shock(cpath("BASE_NS_AMR/JOB_8232/1", True), 'P')
+    plotter.draw(duration=10, block=True)
 
     # import paraview.simple as pvs
     # pvs.SaveState("/d/pseize/pythonstate.pvsm")
--- pvlib.py
+++ pvlib.py
@@ -1,7 +1,9 @@
 import os
-import paraview.simple as pvs
-import utils
+
 import numpy as np
+import paraview.simple as pvs
+
+import utils
 
 DEFAULT_COLORS = ['1f77b4', 'ff7f0e', '2ca02c', 'd62728', '9467bd', '8c564b', 'e377c2', '7f7f7f', 'bcbd22', '17becf']
 
