8dba2d8731300fd30b2697373d90432eb1fd1650
Pierre Seize <pierre.seize@onera.fr>
2022-11-25 12:11:31 +0100

    Add TGV

--- postprocess_package/postprocess/jaguar_tools.py
+++ postprocess_package/postprocess/jaguar_tools.py
@@ -1,4 +1,5 @@
 import collections
+import importlib.resources
 import os
 
 import numpy as np
@@ -42,3 +43,53 @@ def read_error(case_path):
         for tag in data[d]:
             out[d[:-2]][tag] = np.array(data[d][tag])
     return out
+
+
+def read_integral_quantities(case_path):
+    """
+    Read the `sol_IntegralQuantities*.dat` files from the case
+
+    :param str case_path:
+    :return: the values for each saved quantity
+    :rtype: dict
+    """
+    data = collections.defaultdict(lambda: np.ndarray((0,)))
+    files = sorted(utils.fetch_file(os.path.join(case_path, 'sol_IntegralQuantities*.dat')))
+    for file in files:
+        with open(file) as dat_file:
+            labels = [label for label in dat_file.readline().split('"') if label.strip()]
+        file_data = np.loadtxt(file, skiprows=1).T
+        file_data = file_data[:, :-1]  # TODO: last saved value seems wrong
+
+        # TODO: Jaguar saves empty entries with time=0 at the end
+        if file == files[-1]:
+            i, j = 0, file_data.shape[1] - 1
+            while file_data[0, j] == 0:
+                if i == j - 1:
+                    break
+                k = i + (j - i) // 2
+                if file_data[0, k] == 0:
+                    j = k
+                else:
+                    i = k
+            file_data = file_data[:, :j]
+
+        for i, label in enumerate(labels):
+            data[label] = np.concatenate((data[label], file_data[i]))
+    return data
+
+
+def ref_taylor_green_vortex(ref_file=''):
+    """
+    Returns reference data for the Taylor-Green vortex
+
+    :param str ref_file: defaults to '../data/refdata_TGV.dat'
+    :return:
+    :rtype: dict
+    """
+    if not ref_file:
+        ref_file = importlib.resources.files(__package__).joinpath('../data/refdata_TGV.dat')
+    with open(ref_file) as dat_file:
+        labels = [label for label in dat_file.readline().split('"') if label.strip()]
+    data = np.loadtxt(ref_file, skiprows=1).T
+    return {label: data[i] for i, label in enumerate(labels)}
